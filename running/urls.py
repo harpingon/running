145605192a677c8d9e2282cf5e41cc4fbf458f39
Gary Cowell <gary.cowell@gmail.com>
2014-04-17 12:31:52 +0100

    will this work

--- running/urls.py
+++ running/urls.py
@@ -12,9 +12,10 @@ urlpatterns = patterns('',
 
     url(r'^$', TemplateView.as_view(template_name='index.html')),
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^static/(?P<path>.*)$', 'django.views.static.serve', { 'document_root': settings.STATIC_ROOT} ) ,
 )
 
-if not settings.DEBUG:
-    urlpatterns += patterns('', 
-        (r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_ROOT}),
-    )
+#if not settings.DEBUG:
+#    urlpatterns += patterns('', 
+#        (r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_ROOT}),
+#    )
